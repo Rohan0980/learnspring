43276db82f21627957223ac77497736443ca2f1e
Rohan0980 <rohan.maheshwari28@gmail.com>
2020-05-03 17:43:24 +0530

    Initial commit

---
+++ src/test/java/com/rohanm28/learnspring/LearnspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohanm28.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
